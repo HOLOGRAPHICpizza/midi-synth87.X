4000568010130ae173dfa913d0a857fb64bae4ca
Michael Craft <mcraft@peak15.org>
2014-06-01 17:27:22 -0400

    added MIDI setup code

--- main.c
+++ main.c
@@ -3,18 +3,22 @@
 #include <xc.h>
 
 // CONFIG
-#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
-#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
-#pragma config PWRTE = ON       // Power-up Timer Enable bit (PWRT enabled)
-#pragma config BOREN = ON       // Brown-out Reset Enable bit (BOR enabled)
-#pragma config LVP = OFF        // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
-#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
-#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
-#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+#pragma config FOSC = HS		// Oscillator Selection bits (HS oscillator)
+#pragma config WDTE = OFF	   // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = ON	   // Power-up Timer Enable bit (PWRT enabled)
+#pragma config BOREN = ON	   // Brown-out Reset Enable bit (BOR enabled)
+#pragma config LVP = OFF		// Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
+#pragma config CPD = OFF		// Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF		// Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF		 // Flash Program Memory Code Protection bit (Code protection off)
 
 #include "main.h"
 
-void main() {
+void interrupt isr(void) {
+	// interrput code here
+}
+
+void main(void) {
 	// Disable Interrupts
 	INTCONbits.GIE = 0;
 
@@ -23,67 +27,52 @@ void main() {
 	PORTB = 0;
 	PORTC = 0;
 
-    // Switch to bank 1
-    STATUSbits.RP1 = 0;
+	// Switch to bank 1
+	STATUSbits.RP1 = 0;
 	STATUSbits.RP0 = 1;
 
-    // Disable ADC
-    ADCON1 = 0x06;
+	// Disable ADC
+	ADCON1 = 0x06;
 
-    // Disable comparator
+	// Disable comparator
 	CMCONbits.CM0 = 1;
 	CMCONbits.CM1 = 1;
 	CMCONbits.CM2 = 1;
 
-    // Disable T0CKI pin
-    OPTION_REGbits.T0CS = 0;
+	// Disable T0CKI pin
+	OPTION_REGbits.T0CS = 0;
 
-    // set PORT directions
+	// set PORT directions
 	TRISA = 0;					// Set all to output
 	TRISB = 0;
 	TRISC = 0;
 
 	TRISCbits.TRISC7 = 1;		// RC7/RX set to input
 
-    // Switch to bank 0
-    STATUSbits.RP0 = 0;
+	// Switch to bank 0
+	STATUSbits.RP0 = 0;
 
-    // Clear PORTs
+	// Clear PORTs
 	PORTA = 0;
 	PORTB = 0;
 	PORTC = 0;
 
-	/*; Setup USART MIDI Reception
-	movlw	FIFO_BOTTOM
-	movwf	fifo_bottom
-	movwf	fifo_top
-	bsf		fifo_empty,0
-	clrf	note
-	clrf	data_wait
+	// Setup USART MIDI Reception
+	asm("banksel SPBRG");
+	SPBRG = 9;					// 31250 baud
+	TXSTAbits.SYNC = 0;			// clear SYNC
 
-    bcf     STATUS,RP1			; Switch to bank 1
-    bsf     STATUS,RP0
+	asm("banksel RCSTA");
+	RCSTAbits.SPEN = 1;			// set SPEN
 
-	movlw	9					; 31250 baud
-	movwf	SPBRG				; set baud rate generator
+	asm("banksel PIE1");
+	PIE1bits.RCIE = 1;			// enable interrupt on byte RC
 
-	bcf		TXSTA,SYNC			; clear SYNC
-
-	bcf     STATUS,RP0			; back to bank 0
-
-	bsf		RCSTA,SPEN			; set SPEN
-
-	; Switch to bank 1
-    bcf     STATUS,RP1
-    bsf     STATUS,RP0
-	bsf		PIE1,RCIE			; enable interrupt on byte RC
-	; Switch to bank 0
-    bcf     STATUS,RP0*/
-
+	asm("banksel INTCON");
 	INTCONbits.GIE = 1;			// global interrupt enable
 	INTCONbits.PEIE = 1;		//peripheral interrupt enable
-	
-//	bsf		RCSTA,CREN			; set CREN to enable reception
+
+	RCSTAbits.CREN = 1;			// set CREN to enable reception
 
 
 	// Main Loop
